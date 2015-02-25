9210f162a4e1d0400cf254299e343cbfa602a108
Andrew James <andrewpjames87@gmail.com>
2015-02-25 15:47:32 -0700

    User can edit and update products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,19 +8,31 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
-
-    def create
-      @product = Product.new(product_params)
-      if @product.save
-        redirect_to products_path
-      else
-        render :new
-      end
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
     end
+  end
 
     def show
       @product = Product.find(params[:id])
     end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
 
     private
 
